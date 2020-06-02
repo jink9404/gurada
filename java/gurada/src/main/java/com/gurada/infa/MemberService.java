da0523959c526e2ef7c288026aff9896ff60df33
MyeongJin <shinyjade94@gmail.com>
2020-06-02 14:49:08 +0900

    by mj

--- java/gurada/src/main/java/com/gurada/infa/MemberService.java
+++ java/gurada/src/main/java/com/gurada/infa/MemberService.java
@@ -1,11 +1,10 @@
-packa.infa;
+package com.gurada.infa;
 
 import com.gurada.domain.MemberVO;
 
 public interface MemberService {
 	public abstract int userSignUp(MemberVO vo);
 	public abstract MemberVO userSignIn(MemberVO vo);
-	public abstract int userSignUp(MemberVO vo);
 }
 
 
